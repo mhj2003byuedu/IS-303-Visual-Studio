a5b8061f56526a13bd1ac1d13872cf0a59a57f53
Mo Jansson <mhj2003@byu.edu>
2026-03-10 11:59:42 -0600

    c

--- __pycache__/303/dct.py
+++
@@ -1,8 +0,0 @@
-# Dictionaries. Save me
-dctStudent = { "first" : "Greg",
-                "last"  : "Anderson",
-                "id"    : "A12345678"
-                }
-print(dctStudent["first"])
-
-              
